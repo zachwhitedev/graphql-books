29066ecdf5e81963b8e58b1f90938f7e0dea85eb
Zach White <zechnwhite@gmail.com>
2019-12-07 20:03:31 -0800

    strings

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
-app.use('/graphql')
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
