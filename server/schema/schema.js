e564fd0f92cefebc8181c79a2227bdee54b7273c
Zach White <zechnwhite@gmail.com>
2019-12-07 20:51:01 -0800

    adds graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,7 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -9,5 +10,29 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
+})
+
+// dummy data 
+var books = [
+    {name: 'Book One', genre: 'genre one', id: '1'},
+    {name: 'Book Two', genre: 'genre 2', id: '2'},
+    {name: 'Book Three', genre: 'genre 3', id: '3'}
+]
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent,args){
+             return _.find(books, {id: args.id });                
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
 
